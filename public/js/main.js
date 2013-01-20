78e44eece82ac143e1930a6ffab96aa661f83b2c
Serban Carlogea <sherban.carlogea@gmail.com>
2013-01-20 18:57:55 +0100

    added view for individual joke

--- public/js/main.js
+++ public/js/main.js
@@ -11,13 +11,37 @@ function loadJokes() {
     }).done(function ( data ) {
         if(data.jokes){
             $.each(data.jokes, function(key, joke) {
-                $("#jokeTmpl").tmpl(joke).appendTo('#jokeswrapper');
+                $("#jokeTmpl").tmpl(joke).hide().appendTo('#jokeswrapper').fadeIn();
             });
             $('div#loader').hide();
             $('div#showmore').show();
+            $('div#jokeswrapper').show();
         }else{
             $('div#loader').html('<center>No more jokes to show.</center>');
         }
+    });
+}
+
+function loadJoke(id) {
+    var ts = Math.round((new Date()).getTime() / 1000);
+    $('div#loadmoreajaxloader').show();
+    $.ajax({
+        dataType: 'json',
+        url: '/api/jokes/' + id + '?timestamp=' + ts,
+        beforeSend: function ( xhr ) {
+            $('div#loader').show();
+            $('div#showmore').hide();
+            $('div#jokeswrapper').hide();
+            $('div#jokewrapper').html("");
+        }
+    }).done(function ( data ) {
+            if(data.content){
+                $("#jokeTmpl").tmpl(data.content).appendTo('#jokewrapper');
+                $('div#loader').hide();
+                $('div#jokewrapper').fadeIn();
+            }else{
+                $('div#loader').html('<center>No more jokes to show.</center>');
+            }
     });
 }
 
@@ -28,8 +52,30 @@ $(window).scroll(function(){
 });
 
 $(document).ready(function() {
-    loadJokes();
     $("#showmore").click(function() {
         loadJokes();
+    });
+    var hash = window.location.hash.substring(1);
+    if(hash) {
+        loadJoke(hash);
+    } else {
+        loadJokes();
+    }
+    $(window).bind('hashchange', function() {
+        var hash = window.location.hash.substring(1);
+        if(hash) {
+            loadJoke(hash);
+        } else {
+            $('div#jokewrapper').html("");
+            $('div#jokewrapper').hide();
+            $('div#jokeswrapper').html("");
+            loadJokes();
+        }
+    });
+    $(window).on('click', '.joke', function () {
+        var id = $(this).data('tmplItem').data._id;
+        if(id) {
+            window.location.hash = id;
+        }
     });
 });
